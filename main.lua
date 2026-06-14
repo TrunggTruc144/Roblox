9fe9d03774a33f154758e55e76d9769db5f69327
unknown <pnttruc1404@gmail.com>
2026-06-15 03:27:04 +0700

    .

--- main.lua
+++ main.lua
@@ -97,6 +97,7 @@ local function LoadEventRNGConfig()
 	getgenv().GDO_BACKROOMS = true
 	getgenv().GBACKROOMS_OPEN_CHESTS = true
 	getgenv().GBACKROOMS_MIN_MULTIPLIER = 75
+	getgenv().GALLOW_HOPPING = true
 
 	getgenv().GAUTO_TRADE_WHITE_LIST = PRIVATE_CONFIG.MAIL_RECEIVERS or {}
 	getgenv().GTRADE_ITEMS = {
