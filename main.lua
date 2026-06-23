81c33705d5078012c5a35bd0ca291ff915eac270
unknown <pnttruc1404@gmail.com>
2026-06-23 17:12:34 +0700

    .

--- main.lua
+++ main.lua
@@ -71,9 +71,9 @@ end
 local function LoadEventRNGConfig()
 	getgenv().GPROGRESS_MODE = "Hybrid"
 	getgenv().GRANK_FIRST = true
-	getgenv().GRANK_TO = 11
-	getgenv().GZONE_TO = 999
-	getgenv().GREBIRTH_TO = 9
+	getgenv().GRANK_TO = 8
+	getgenv().GZONE_TO = 1
+	-- getgenv().GREBIRTH_TO = 9
 	getgenv().GGFX_MODE = 1
 	getgenv().GMASTERY_TO_MAX = "Eggs"
 
@@ -110,21 +110,20 @@ local function LoadEventRNGConfig()
 	}
 
 	getgenv().GDO_BACKROOMS = true
-	getgenv().GALLOW_HOPPING = false
+	getgenv().GALLOW_HOPPING = true
 	getgenv().GBACKROOMS_MIN_MULTIPLIER = 100
-	getgenv().GBACKROOMS_DO_BOSS_ROOM = true
-	getgenv().GBACKROOMS_COINS_TO_FARM_BEFORE_HATCHING = "1000m"
-	getgenv().GBACKROOMS_TOKEN_CHEST_FARM = true
+	getgenv().GBACKROOMS_DO_BOSS_ROOM = false
+	getgenv().GBACKROOMS_COINS_TO_FARM_BEFORE_HATCHING = "1500m"
+
 	getgenv().GEVENT_UPGRADES = {
-		{ Id = "BackroomsBossDamage", MaxTier = 1 }, --max is 25
-		{ Id = "BackroomsTokenFind", MaxTier = 1 }, --max is 20
-		{ Id = "BackroomsExtraLootRoll", MaxTier = 1 }, --max is 20
-		{ Id = "BackroomsCoinMultiplier", MaxTier = 5 }, --max is 20
-		{ Id = "BackroomsEggLuck", MaxTier = 5 }, --max is 20
-		{ Id = "BackroomsKeyFind", MaxTier = 10 }, --max is 15,
-		{ Id = "BackroomsDeepBossDamage", MaxTier = 1 }, --max is 25,
+		{ Id = "BackroomsCoinMultiplier", MaxTier = 7 }, --max is 20,
+		{ Id = "BackroomsEggLuck", MaxTier = 7 }, --max is 20,
+		{ Id = "BackroomsKeyFind", MaxTier = 7 }, --max is 15,
+		{ Id = "BackroomsDeepBossDamage", MaxTier = 3 }, --max is 25,
 	}
 
+	getgenv().GBACKROOMS_TOKEN_CHEST_FARM = true
+	getgenv().GBACKROOMS_DO_BACKROOMS_DELETE = true
 	getgenv().GBACKROOMS_OPEN_CHESTS = false
 	getgenv().GBACKROOMS_OPEN_CHESTS_ONLY_DEEP_ROOMS = false
 	getgenv().GBACKROOMS_KICK_AFTER_CHESTS = false
@@ -146,14 +145,14 @@ local function LoadEventRNGConfig()
 	ApplyCommonMailConfig()
 end
 
--- if game.PlaceId ~= 8737899170 then
--- 	pcall(function()
--- 		game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("World1Teleport"):InvokeServer()
--- 	end)
+if game.PlaceId ~= 8737899170 then
+	pcall(function()
+		game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("World1Teleport"):InvokeServer()
+	end)
 
--- 	task.wait(20)
--- 	return
--- end
+	task.wait(20)
+	return
+end
 
 LoadEventRNGConfig()
 
