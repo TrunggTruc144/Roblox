b0efb3f73251ce1a812104726a8f8db8ad61d979
unknown <pnttruc1404@gmail.com>
2026-06-29 07:01:56 +0700

    .

--- main.lua
+++ main.lua
@@ -66,14 +66,14 @@ local function ApplyCommonMailConfig()
 		["Magic Shard"] = { Class = "Misc", Id = "Magic Shard", MinAmount = 200 },
 		["Bucket O' Magic"] = { Class = "Misc", Id = "Bucket O' Magic", MinAmount = 200 },
 
-		["Soccer Gift"] = { Class = "Lootbox", Id = "Soccer Gift", Amount = 200 },
+		["Soccer Gift"] = { Class = "Lootbox", Id = "Soccer Gift", Amount = 300 },
 	}
 end
 
 local function LoadEventRNGConfig()
 	getgenv().GPROGRESS_MODE = "Hybrid"
 	getgenv().GRANK_FIRST = true
-	getgenv().GRANK_TO = 6
+	getgenv().GRANK_TO = 5
 	getgenv().GZONE_TO = 99
 	-- getgenv().GREBIRTH_TO = 9
 	getgenv().GGFX_MODE = 1
